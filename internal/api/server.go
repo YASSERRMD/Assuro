696242a17bfb96facab17850d9907b7ada7ad889
YASSERRMD <arafath.yasser@gmail.com>
2026-05-17 22:49:10 +0400

    feat: add CORS middleware to allow frontend access

--- internal/api/server.go
+++ internal/api/server.go
@@ -12,6 +12,7 @@ import (
 	"github.com/YASSERRMD/Assuro/internal/store"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 	"go.uber.org/zap"
 )
 
@@ -55,6 +56,14 @@ func NewServer(addr string, logger *zap.Logger, opts ...ServerOption) *Server {
 	r.Use(middleware.RealIP)
 	r.Use(middleware.Recoverer)
 	r.Use(middleware.Timeout(30 * time.Second))
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:3000", "http://localhost:3001"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
 	r.Use(requestLogger(logger))
 
 	r.Get("/healthz", s.healthz)
